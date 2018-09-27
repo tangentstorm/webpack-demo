a70aa44bef38d4202817d521ac6ab1142805d8e2
tangentstorm <michal.wallace@gmail.com>
2018-09-27 18:58:28 -0400

    packing css and png assets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './tangentstorm.png';
 
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  let img = new Image(); img.src = Icon;
+  element.appendChild(img);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
+    path: path.resolve(__dirname, 'dist') },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: ['style-loader', 'css-loader'] },
+      { test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'] }]}};
